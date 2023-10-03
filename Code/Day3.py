7c27e4be8a5f9199665a5157ccf04121c745816e
Zayintech <Zavionconner803@gmail.com>
2023-10-02 20:33:40 -0400

    Update Day3.py

--- Code/Day3.py
+++ Code/Day3.py
@@ -1,10 +1,13 @@
 #Treaure Island project
 print("Welcome to Treasure Island. Your mission is to find the treasure.")
 direction = input("Would you like to go to the left or right. \n")
+# allow the user to choose which direction they would like to go.
 if direction == "left":
     sORw = input("You've came accross a river, would you like to 'swim' or 'wait'? \n")
+    # Wait or swim to their death
     if sORw == "wait":
         road = input("You've came accross three roads; yellow. red and blue")
+        # user picks their final path
         if road == "yellow":
              print("You win")
         elif road == "blue":
@@ -17,7 +20,3 @@ if direction == "left":
          print("Attacked by gator.\n GAME OVER")
 else: 
     print("Fall into hole and die.\n GAME OVER")
-
-
-
-
