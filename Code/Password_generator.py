ad029b5937b023080d19908ec34435b8e13c1b51
Zayintech <Zavionconner803@gmail.com>
2023-10-05 20:13:22 -0400

    Update Password_generator.py

--- Code/Password_generator.py
+++ Code/Password_generator.py
@@ -25,6 +25,6 @@ for num in range(1, nr_numbers +1):
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-print(f"Your password is {password}")
+print(f"Your password is :{password}")
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
